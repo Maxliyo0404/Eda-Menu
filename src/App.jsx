1e8d21cbca15194a898949bd4172b4cc90937750
Mahliyo <gasandigital13@gmail.com>
2026-06-13 09:05:58 +0500

    Eda Menu star

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import "./App.css";
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Componenets/Layout/Layout";
-import 
+import MenuPages from "./Pages/MenuPages/MenuPages";
+
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
      <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout/>}>
-        <Route path="menu" element = {<M/>}/>
+        <Route path="menu" element = {<MenuPages/>}/>
         <Route path="news" element = {<ServicesPages/>}/>
         <Route path="about" element = {<CompanyPages/>}/>
 
