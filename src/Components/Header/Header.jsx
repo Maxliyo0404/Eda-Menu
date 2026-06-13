b8ee139331f4426c7468cbf3a0c9dcf6a0b732cd
Mahliyo <gasandigital13@gmail.com>
2026-06-13 10:04:56 +0500

    header star

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,10 @@
 import { useTranslation } from "react-i18next";
 import "./Header.css";
-import React from 'react'
-import logos_1 from "./image/logos_1.png"
-import { Link } from "react-router-dom";
 import React, { useState } from 'react'
-
+import { Link } from "react-router-dom";
 function Header() {
-  const [t, i18n] = useTranslation();
-  const [isMenuOpen, setIsMenuOpen] = u ;
+    const [t, i18n] = useTranslation();
+    const [isMenuOpen, setIsMenuOpen] = useState  (false);
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
@@ -15,34 +12,7 @@ function Header() {
         i18n.changeLanguage(event.target.value)
     };
   return (
-    <div className="header">
-      <div className="container">
-        <div className="header-container">
-             <a className="logo" href="#"><img src={logos_1} alt="rasm" /></a>
-                 <nav className={`header-nav ${isMenuOpen ? "active" : ""}`}>
-                <ul className="header-list">
-                    <li><Link className="header-link" to="home">{t("header.menu")}</Link></li>
-                    <li><Link className="header-link" to="services">{t("header.news")}</Link></li>
-                    <li><Link className="header-link" to="company">{t("header.about")}</Link></li>
-                    <li><Link className="header-link" to="career" onClick={(e)=> e.preventDefault()}>{t("header.Contact Us")}</Link></li>
-                    <button type="submit" className="header-btn">{t("header.header-btn")}</button>
-                </ul>
-                <div className="header-actions">
-                    <select className="select" onChange={handleChangeLanguage} value={i18n.language}>
-                      <option value="uz">Uz</option>
-                      <option value="en">Eng</option>
-                      <option value="ru"> Rus</option>
-                    </select>
-                  </div>
-           </nav>
-      <div className={`burger-menu ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-        </div>
-      </div>
-    </div>
+   
   )
 }
 
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,11 +2,12 @@ import "./Layout.css";
 import React from 'react'
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
-
+import Header from "../Header/Header";
 function Layout() {
   return (
     <div className="layout">
       <main>
+        <Header/>
         <Outlet/>
         <Footer/>
       </main>
