fb8510355f88b19d170d2229384af34c31980b01
Mahliyo <gasandigital13@gmail.com>
2026-06-13 08:41:57 +0500

    Eda Menu star

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./Pages/MenuPages/MenuPages";
+
 
 function App() {
   return (
     <>
-    
-    
+     <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route path="menu" element = {<Menu/>}/>
+        <Route path="news" element = {<ServicesPages/>}/>
+        <Route path="about" element = {<CompanyPages/>}/>
+
+      </Route>
+    </Routes>
+       </BrowserRouter>
+
     </>
   )
+   
 }
 
 export default App
