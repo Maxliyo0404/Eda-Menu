941d50b574a10668f01320376b3707352a8eba43
Mahliyo <gasandigital13@gmail.com>
2026-06-14 07:56:15 +0500

    sectionFour css

--- src/Components/Main/SectionFive/SectionFive.jsx
+++ src/Components/Main/SectionFive/SectionFive.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './SectionFive.css';
 
 const SectionFive = () => {
-  // YouTube video ID sini o'zgartiring (video URL dagi v=... qismidan keyingi belgilarni oling)
-  const videoId = "your-video-id-here"; 
+  // Bu yerda videoni ID sini o'zgartirishingiz mumkin
+  const videoId = "k4tDq397Sfk"; 
 
   return (
     <section id="sectionFive" className="video-section">
       <div className="video-container">
         <iframe
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=0&mute=1&loop=1&playlist=${videoId}`}
-          title="YouTube video player"
+          src={`https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1&autoplay=0`}
+          title="Restaurant Promotional Video"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
