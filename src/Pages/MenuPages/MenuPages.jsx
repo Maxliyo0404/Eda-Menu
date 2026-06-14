d00fec0e65828249c20e5329f734970f501e6369
Mahliyo <gasandigital13@gmail.com>
2026-06-14 07:52:19 +0500

    sectionFour css

--- src/Pages/MenuPages/MenuPages.jsx
+++ src/Pages/MenuPages/MenuPages.jsx
@@ -5,6 +5,7 @@ import SectionOne from "../../Components/Main/SectionOne/SectionOne";
 import SectionTwo from "../../Components/Main/SectionTwo/SectionTwo";
 import SectionThree from "../../Components/Main/SectionThree/SectionThree";
 import SectionFour from "../../Components/Main/SectionFour/SectionFour";
+import SectionFive from "../../Components/Main/SectionFive/SectionFive";
 function MenuPages() {
   return (
     <>
@@ -13,6 +14,7 @@ function MenuPages() {
      <SectionTwo/>
      <SectionThree/>
      <SectionFour/>
+     <SectionFive/>
     </>
   )
 }
