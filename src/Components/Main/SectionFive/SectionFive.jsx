8219aa00eefbdd04546c74263bd3a89157f9da55
Mahliyo <gasandigital13@gmail.com>
2026-06-14 08:01:04 +0500

    fnish

--- src/Components/Main/SectionFive/SectionFive.jsx
+++ src/Components/Main/SectionFive/SectionFive.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './SectionFive.css';
 
 const SectionFive = () => {
-  // Bu yerda videoni ID sini o'zgartirishingiz mumkin
+  // Bu video restoranlar uchun mos (sifatli klip)
   const videoId = "k4tDq397Sfk"; 
 
   return (
     <section id="sectionFive" className="video-section">
       <div className="video-container">
         <iframe
-          src={`https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1&autoplay=0`}
+          src={`https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1&autoplay=1&mute=1&loop=1&playlist=${videoId}`}
           title="Restaurant Promotional Video"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
