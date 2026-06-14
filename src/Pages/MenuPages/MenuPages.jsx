6542769dfcce7135546dbd654e1a591bd88d1532
Mahliyo <gasandigital13@gmail.com>
2026-06-14 06:28:47 +0500

    sectionOne star wrapper css responsive fnish

--- src/Pages/MenuPages/MenuPages.jsx
+++ src/Pages/MenuPages/MenuPages.jsx
@@ -2,12 +2,13 @@ import "./MenuPages.css";
 import React from 'react'
 import Hero from "../../Components/Hero/Hero";
 import SectionOne from "../../Components/Main/SectionOne/SectionOne";
-
+import SectionTwo from "../../Components/Main/SectionTwo/SectionTwo";
 function MenuPages() {
   return (
     <>
      <Hero/>
      <SectionOne/>
+     <SectionTwo/>
     </>
   )
 }
