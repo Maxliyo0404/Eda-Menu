cea910e2ed228e206b64cd870ef69056d96b31d9
Mahliyo <gasandigital13@gmail.com>
2026-06-14 07:18:40 +0500

    sectionThree

--- src/Pages/MenuPages/MenuPages.jsx
+++ src/Pages/MenuPages/MenuPages.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Hero from "../../Components/Hero/Hero";
 import SectionOne from "../../Components/Main/SectionOne/SectionOne";
 import SectionTwo from "../../Components/Main/SectionTwo/SectionTwo";
+import SectionThree from "../../Components/Main/SectionThree/SectionThree";
 function MenuPages() {
   return (
     <>
      <Hero/>
      <SectionOne/>
      <SectionTwo/>
+     <SectionThree/>
     </>
   )
 }
