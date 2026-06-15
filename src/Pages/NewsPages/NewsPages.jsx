945a90328b98324bd9cc6581d4c4dd0a81c69ebc
Mahliyo <gasandigital13@gmail.com>
2026-06-15 08:09:10 +0500

    footer wrrapper

--- src/Pages/NewsPages/NewsPages.jsx
+++ src/Pages/NewsPages/NewsPages.jsx
@@ -1,10 +1,41 @@
-import "./NewsPages.css";
-import React from 'react'
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import './NewsPages.css';
 
-function NewsPages() {
+const News = () => {
+  const { t, i18n } = useTranslation();
+  const categories = t("news.categories", { returnObjects: true });
+  const [active, setActive] = useState(categories[0]);
+
   return (
-    <div>News</div>
-  )
-}
+    <section className="news-section">
+      <aside className="news-sidebar">
+        <h2>{t("news.title")}</h2>
+        <div className="news-filters">
+          {categories.map((cat) => (
+            <button 
+              key={cat} 
+              className={active === cat ? 'active' : ''} 
+              onClick={() => setActive(cat)}
+            >
+              {cat}
+            </button>
+          ))}
+        </div>
+      </aside>
 
-export default NewsPages
+      <main className="news-grid">
+        {[1, 2, 3, 4, 5, 6].map((item) => (
+          <div key={item} className="news-card">
+            <div className="news-img"></div>
+            <h3>Barbecue Shish kebab Shashlik Skewer</h3>
+            <p className="news-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button className="read-more">{t("news.readMore")}</button>
+          </div>
+        ))}
+      </main>
+    </section>
+  );
+};
+
+export default News;
