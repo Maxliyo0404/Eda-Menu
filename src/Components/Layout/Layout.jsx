5994fa0dc803189025e035c31b21034c980ca8e6
Mahliyo <gasandigital13@gmail.com>
2026-06-15 07:05:11 +0500

    footer

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,15 +1,15 @@
 import "./Layout.css";
 import React from 'react'
 import { Outlet } from "react-router-dom";
-import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+
 function Layout() {
   return (
     <div className="layout">
       <main>
         <Header/>
         <Outlet/>
-        <Footer/>
+     
       </main>
     </div>
   )
