5ec51a41d2c261d64c1ae7b4a9676f29af8a7b34
Mahliyo <gasandigital13@gmail.com>
2026-06-16 19:24:37 +0500

    newsPages star wrapper css responsive fnish

--- src/Components/Main3/Hero3/Hero3.jsx
+++ src/Components/Main3/Hero3/Hero3.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import "./Hero3.css";
 import React from 'react'
+import olov from "./image/olov.png";
 
 function Hero3() {
     const {t} = useTranslation();
@@ -8,7 +9,7 @@ function Hero3() {
     <div className="hero3">
         <div className="container">
             <div className="hero3-info">
-                
+
             </div>
         </div>
     </div>
