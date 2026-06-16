cf583d799d2e40b2557e07b913dd7d7bac4ffbab
Mahliyo <gasandigital13@gmail.com>
2026-06-16 19:54:55 +0500

    about star wrapper css fnish

--- src/Pages/ContactPages/CantactPages.jsx
+++ src/Pages/ContactPages/CantactPages.jsx
@@ -1,10 +1,10 @@
 import "./ContactPages.css";
 import React from 'react'
-
+import Hero4 from "../../Components/Main4/Hero4/Hero4";
 function CantactPages() {
   return (
    <>
-   
+   <Hero4/>
    </>
   )
 }
