9976ad1c0726bd02b8247f1485e60f48f13a42ae
Mahliyo <gasandigital13@gmail.com>
2026-06-16 20:36:32 +0500

    contact star

---
+++ src/Components/Main4/SectionOne4/SectionOne4.jsx
@@ -0,0 +1,16 @@
+import { useTranslation } from "react-i18next";
+import "./SectionOne4.css";
+import React from 'react'
+
+function SectionOne4() {
+    const {t} = useTranslation();
+  return (
+    <div className="sectionOne4">
+        <div className="container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default SectionOne4
--- src/Pages/ContactPages/CantactPages.jsx
+++ src/Pages/ContactPages/CantactPages.jsx
@@ -1,10 +1,12 @@
 import "./ContactPages.css";
 import React from 'react'
 import Hero4 from "../../Components/Main4/Hero4/Hero4";
+import SectionOne4 from "../../Components/Main4/SectionOne4/SectionOne4";
 function CantactPages() {
   return (
    <>
    <Hero4/>
+   <SectionOne4/>
    </>
   )
 }
